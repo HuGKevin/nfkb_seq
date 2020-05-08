d81dfa743d5f88e7360a8751bc06bebcad5e983c
HuGKevin <kevin.hu@yale.edu>
2020-05-08 12:15:39 -0400

    pooling reads job array added

---
+++ src/pool_reads.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --job-name=pool_reads.%a
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=10gb
+#SBATCH -o /home/kh593/scratch60/nfkb_seq/logs/pool_reads%a.out
+#SBATCH -e /home/kh593/scratch60/nfkb_seq/logs/pool_reads%a.err
+#SBATCH --array=1-453
+
+module purge
+module load SAMtools
+
+index_file="/home/kh593/project/nfkb_seq/data/pooling_array.tsv"
+
+
+### job information
+donor=$(awk -F'\t' -v row=${SLURM_ARRAY_TASK_ID} -v num=1 'FNR == row {print $num}' $index_file)
+expt=$(awk -F'\t' -v row=${SLURM_ARRAY_TASK_ID} -v num=2 'FNR == row {print $num}' $index_file)
+stim=$(awk -F'\t' -v row=${SLURM_ARRAY_TASK_ID} -v num=3 'FNR == row {print $num}' $index_file)
+lib=$(awk -F'\t' -v row=${SLURM_ARRAY_TASK_ID} -v num=4 'FNR == row {print $num}' $index_file)
+
+copas=$(grep "${donor}" /home/kh593/project/nfkb_seq/data/pooling_index.tsv |
+	    grep "${expt}" | 
+	    grep "${stim}" |
+	    cut -f5 |
+	    sed "s/$/.final.bam/g" |
+	    sed "s/^/\/home\/kh593\/scratch60\/nfkb_seq\/aligned_reads\//g")
+echo "${donor} ${expt} ${stim} ${lib}"
+echo $copas
+    
+samtools merge -@ 16 /home/kh593/scratch60/nfkb_seq/pooled_reads/${lib}_pooled.bam $copas
+echo "${lib} completed"
