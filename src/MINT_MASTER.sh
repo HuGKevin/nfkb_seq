730bac88a22926db2cb50a7339d7c5e5689d0877
HuGKevin <kevin.hu@yale.edu>
2020-06-19 18:33:25 -0400

    added cluster binding to master script

--- src/MINT_MASTER.sh
+++ src/MINT_MASTER.sh
@@ -510,8 +510,29 @@ rm ${abc}
 
 #####################################################################
 
-################## 
-sbatch /home/kh593/project/nfkb_seq/src/bind_clusters.sh
+################## Convert MCL output to BED file #########################
+#!/bin/bash
+# Usage: sbatch bind_clusters.sh
+
+#SBATCH --job-name=bind_clusters%a
+#SBATCH --partition=general
+#SBATCH --mem=25gb --cpus-per-task=1
+#SBATCH -e /home/kh593/scratch60/nfkb_seq/logs/bind_clusters%a.err
+#SBATCH -o /home/kh593/scratch60/nfkb_seq/logs/bind_clusters%a.out
+#SBATCH --array=1-24
+
+module purge
+
+module load R/3.6.1-foss-2018b
+
+index="/home/kh593/project/nfkb_seq/data/mcl_index.tsv"
+
+chr=$(awk -F"\t" -v row=${SLURM_ARRAY_TASK_ID} -v num=1 'FNR == row {print $num}' $index)
+expt=$(awk -F"\t" -v row=${SLURM_ARRAY_TASK_ID} -v num=2 'FNR == row {print $num}' $index)
+
+Rscript /home/kh593/project/nfkb_seq/src/bind_clusters.R ${expt} ${chr}
+
+echo "Job completed"
 
 #####################################################################
 ### Generate binding profile for each DNA library
